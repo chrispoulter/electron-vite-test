7bd67612148b4d78293134339e3b81beae74be55
Chris Poulter <cpoulter@hotmail.co.uk>
2026-03-08 13:04:03 +0000

    fix: update copyright year in Footer component

--- src/renderer/src/components/Footer.tsx
+++ src/renderer/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = (): React.JSX.Element => {
 
   return (
     <footer className="bg-gray-800 text-white p-4 mt-auto">
-      <p className="text-center">&copy; 2024 Media Library. All rights reserved.</p>
+      <p className="text-center">&copy; 2026 Media Library. All rights reserved.</p>
       <p className="text-center text-sm mt-2">
         Electron v{versions.electron} | Chromium v{versions.chrome} | Node v{versions.node}
       </p>
