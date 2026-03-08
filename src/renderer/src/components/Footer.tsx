bf6efbe90f6e5ee2c30134fcf77af8900bf20199
Chris Poulter <cpoulter@hotmail.co.uk>
2026-03-08 15:51:14 +0000

    refactor: simplify Footer component by removing version display and updating copyright year

--- src/renderer/src/components/Footer.tsx
+++ src/renderer/src/components/Footer.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-
 export const Footer = (): React.JSX.Element => {
-  const [versions] = useState(window.electron.process.versions)
-
   return (
-    <footer className="mt-auto bg-gray-800 p-4 text-white">
-      <p className="text-center">&copy; 2026 Media Library. All rights reserved.</p>
-      <p className="mt-2 text-center text-sm">
-        Electron v{versions.electron} | Chromium v{versions.chrome} | Node v{versions.node}
-      </p>
+    <footer className="mt-auto bg-gray-800 p-4 text-center text-white">
+      &copy; {new Date().getFullYear()} Media Library. All rights reserved.
     </footer>
   )
 }
