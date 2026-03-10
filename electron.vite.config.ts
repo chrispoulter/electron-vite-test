636e8663329ef2edcde11ed244eb6e1021aab15e
Chris Poulter <cpoulter@hotmail.co.uk>
2026-03-10 12:13:19 +0000

    feat: add commented build configuration to main in electron.vite.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  main: {},
+  main: {
+    // build: {
+    //   watch: {}
+    // }
+  },
   preload: {},
   renderer: {
     resolve: {
